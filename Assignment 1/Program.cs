d93df13bff7b80a976c39a4802ac0c7869b1a5e0
Justin Lim <justinlim.dn98@gmail.com  git config --global user.git config --global user.email justinlim.dn98@gmail.comgit config --glocal user.name LimJustin0716>
2021-10-22 09:51:20 +0800

    first commit

---
+++ Assignment 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace Assignment1
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            string message = "Justin Espeña Lim";
+            string tittle = "Name";
+            MessageBox.Show(message, tittle);
+        }
+    }
+}
